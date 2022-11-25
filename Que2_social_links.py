836ffb1356f06e4aec8ac9086c1f0af6a8eed666
sanketdkale <91241400+sanketdkale@users.noreply.github.com>
2022-11-25 15:52:01 +0530

    Add files via upload
    WAP to get the Social Links, Email & Contacts details of a website on user input.
    Input:
    https://ful.io
    Output:
    Social links -
    https://www.facebook.com/fulioTech/
    https://www.linkedin.com/company/ful-io/
    Email/ssupport@ful.io
    Contact:
    +1 343 303 6668

---
+++ Que2_social_links.py
@@ -0,0 +1,65 @@
+import requests
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
+import colorama
+
+colorama.init()
+GREEN = colorama.Fore.GREEN
+GRAY = colorama.Fore.LIGHTBLACK_EX
+RESET = colorama.Fore.RESET
+YELLOW = colorama.Fore.YELLOW
+
+# initialize the set of links (unique links)
+internal_urls = set()
+external_urls = set()
+
+
+def is_valid(url):
+    parsed = urlparse(url)
+    return bool(parsed.netloc) and bool(parsed.scheme)
+
+
+def get_all_website_links(url):
+    urls = set()
+    domain_name = urlparse(url).netloc
+    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+
+    for a_tag in soup.findAll("a"):
+        href = a_tag.attrs.get("href")
+        if href == "" or href is None:
+            continue
+
+        href = urljoin(url, href)
+        parsed_href = urlparse(href)
+        href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
+
+        if not is_valid(href):
+            continue
+
+        if href in internal_urls:
+            continue
+
+        print(f"{GREEN}[*] Internal link: {href}{RESET}")
+        urls.add(href)
+        internal_urls.add(href)
+    return urls
+
+
+# number of urls visited so far will be stored here
+total_urls_visited = 0
+
+
+def crawl(url, max_urls=30):
+    global total_urls_visited
+    total_urls_visited += 1
+    print(f"{YELLOW}[*] Crawling: {url}{RESET}")
+    links = get_all_website_links(url)
+    for link in links:
+        if total_urls_visited > max_urls:
+            break
+        crawl(link, max_urls=max_urls)
+
+
+if __name__ == "__main__":
+    crawl("https://ful.io/")
+    print("[+] Total Internal links:", len(internal_urls))
